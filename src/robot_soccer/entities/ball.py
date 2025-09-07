10468952c5893a29457821a39c591f5be70c892c
YohaniIsaac <yohani.tripai.yt@gmail.com>
2025-09-07 15:09:00 -0300

    feat: Captures the speed of the ball through a difference in position over time

--- src/robot_soccer/entities/ball.py
+++ src/robot_soccer/entities/ball.py
@@ -4,6 +4,7 @@ Este módulo contiene la clase Ball que representa la pelota en el juego,
 incluyendo su posición y métodos básicos de manipulación.
 """
 
+import time
 import numpy as np
 
 
@@ -18,6 +19,11 @@ class Ball:
     Attributes:
         x (int): Coordenada X de la pelota en el campo.
         y (int): Coordenada Y de la pelota en el campo.
+        position_history (list): Historial de posiciones de la pelota.
+        velocity_x (float): Velocidad de la pelota en el eje x.
+        velocity_y (float): Velocidad de la pelota en el eje y.
+        speed (float): Rapidez de la pelota (magnitud).
+        last_update (float): Última actualización.
     """
 
     def __init__(self, x, y):
@@ -33,6 +39,11 @@ class Ball:
         """
         self.x = x
         self.y = y
+        self.position_history = []
+        self.velocity_x = 0.0
+        self.velocity_y = 0.0
+        self.speed = 0.0
+        self.last_update = 0.0
 
     def get_position(self):
         """Obtiene la posición actual de la pelota.
@@ -42,6 +53,15 @@ class Ball:
                           El array es de tipo float64 y shape (2,).
         """
         return np.array([self.x, self.y])
+
+    def get_velocity(self):
+        """Obtiene la posición actual de la pelota.
+
+        Returns:
+            numpy.ndarray: Array de numpy conteniendo las velocidades y rapidez [v_x, v_y, spped]
+            de la pelota. El array es de tipo float64 y shape (3,).
+        """
+        return np.array([self.velocity_x, self.velocity_y, self.speed])
 
     def set_position(self, x, y):
         """Establece una nueva posición para la pelota.
@@ -61,3 +81,26 @@ class Ball:
         """
         self.x = x
         self.y = y
+        self.update_velocity()
+
+
+    def update_velocity(self):
+        """Calcular velocidad basada en historial."""
+        current_time = time.time()
+
+
+        if self.position_history and current_time - self.last_update > 0.05:  # Cada 50ms
+            prev_x, prev_y, prev_time = self.position_history[-1]
+            dt = current_time - prev_time
+
+            if dt > 0:
+                self.velocity_x = (self.x - prev_x) / dt
+                self.velocity_y = (self.y - prev_y) / dt
+                self.speed = (self.velocity_x**2 + self.velocity_y**2)**0.5
+
+        # Mantener historial (máximo 10 posiciones)
+        self.position_history.append((self.x, self.y, current_time))
+        if len(self.position_history) > 5:
+            self.position_history.pop(0)
+
+        self.last_update = current_time
